1d3509d372ad533464530804ea0dcc7a4fc7f5f4
yumetia <yumetia000@gmail.com>
2026-03-10 19:56:04 +0100

    reset: stable sanity api

--- src/locales/en/en.tsx
+++ src/locales/en/en.tsx
@@ -1,6 +1,6 @@
 
 
-import aboutSections from "./aboutSections";
+import { LetterText, CalendarSync, Paintbrush } from "lucide-react";
 
 import age from "@utils/data";
 
@@ -27,7 +27,27 @@ const en = {
   },
   about: {
     title: "About",
-    sections: aboutSections,
+      sections: [{
+      id: 1,
+      title: "Frontend Developer",
+      description:
+        "I am a frontend developer with solid experience in building interactive and responsive web applications.",
+      icon: LetterText,
+    },
+    {
+      id: 2,
+      title: "Backend Developer",
+      description:
+        "I have a strong foundation in backend development, designing robust APIs and handling database management.",
+      icon: CalendarSync,
+    },
+    {
+      id: 3,
+      title: "UI/UX Enthusiast",
+      description:
+        "Crafting visually appealing and user-friendly interfaces is one of my top priorities.",
+      icon: Paintbrush,
+    },],
   },
   experiences: {
     title: "Experiences",
--- src/locales/ja/ja.tsx
+++ src/locales/ja/ja.tsx
@@ -1,5 +1,5 @@
 
-import aboutSections from "./aboutSections";
+import { LetterText, CalendarSync, Paintbrush } from "lucide-react";
 
 const ja = {
   navbar: {
@@ -23,7 +23,29 @@ const ja = {
   },
   about: {
     title: "自己紹介",
-    sections: aboutSections,
+    sections: [
+      {
+        id: 1,
+        title: "フロントエンド開発者",
+        description:
+          "インタラクティブでレスポンシブなWebアプリケーションの構築において、確かな経験を持つフロントエンド開発者です。",
+        icon: LetterText,
+      },
+      {
+        id: 2,
+        title: "バックエンド開発者",
+        description:
+          "バックエンド開発における強固な基盤を持ち、堅牢なAPI設計やデータベース管理を行っています。",
+        icon: CalendarSync,
+      },
+      {
+        id: 3,
+        title: "UI/UX愛好家",
+        description:
+          "視覚的に魅力的でユーザーフレンドリーなインターフェースの設計を最優先事項の一つとしています。",
+        icon: Paintbrush,
+      },
+    ],
   },
   experiences: {
     title: "経験",
