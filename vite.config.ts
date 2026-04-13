acafb27b48e367ccd3e93e2ab2b5bf7d7da8d30e
yumetia <yumetia000@gmail.com>
2026-04-13 16:14:13 +0200

    bundle(vite): mangle Tailwind classes in production bundles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import utwm from 'unplugin-tailwindcss-mangle/vite';
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -18,7 +19,10 @@ export default defineConfig({
       "@lib": path.resolve(__dirname, "src/lib"),
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), 
+    tailwindcss(),
+    utwm()
+  ],
   define: {
     "process.env":{}
   },
